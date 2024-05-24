dbab7dfa943d6458a1e5a86c661902b33dfc9fb7
dhanushkasampath <dhanushka.2018410@iit.ac.lk>
2024-05-24 07:59:32 +0530

    first commit

---
+++ src/main/java/com/learn/db_querying_techniques/DbQueryingTechniquesApplication.java
@@ -0,0 +1,13 @@
+package com.learn.db_querying_techniques;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DbQueryingTechniquesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DbQueryingTechniquesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/learn/db_querying_techniques/DbQueryingTechniquesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learn.db_querying_techniques;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DbQueryingTechniquesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
